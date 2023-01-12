2c0dfd9c9c5cc891072287d44dd050dc235a44f3
成玉仁 <martian-cheng@live.com>
2023-01-12 22:50:21 +0800

    添加GameDebug

---
+++ Assets/Program/GameCore/GameConfig/EnumDefine.cs
@@ -0,0 +1,27 @@
+﻿namespace GameCore
+{
+    public enum EDebugLevel
+    {
+        /// <summary>
+        /// 生产环境
+        /// </summary>
+        Prod,
+        /// <summary>
+        /// 开发环境
+        /// </summary>
+        Dev,
+    }
+    /// <summary>
+    /// 宏定义类型
+    /// </summary>
+    public enum EDefineType
+    {
+        UNITY_EDITOR,
+        UNITY_STANDALONE_WIN,
+        UNITY_IPHONE,
+        UNITY_ANDROID,
+        UNITY_WEBGL,
+        DEBUG,
+    }
+    
+}
---
+++ Assets/Program/GameCore/GameConfig/GameConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace GameCore
+{
+    public class GameConfig
+    {
+        /// <summary>
+        /// Debug标记
+        /// </summary>
+        public static EDebugLevel DebugLevel
+        {
+            get
+            {
+#if ENABLE_DEBUG
+                return EDebugLevel.Dev;
+#endif
+                return EDebugLevel.Prod;
+            }
+        }
+
+    }
+}
