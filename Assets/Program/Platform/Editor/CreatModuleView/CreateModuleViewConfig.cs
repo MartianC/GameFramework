4ef3a14a615a560fdaec0823c22c63df99504ce3
成玉仁 <martian-cheng@live.com>
2023-06-29 23:13:05 +0800

    添加CreateModuleViewConfig

---
+++ Assets/Program/Platform/Editor/CreatModuleView/CreateModuleViewConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platform
+{
+    public class CreateModuleViewConfig
+    {
+        public const string PrefabStart = "MV_";
+        public const string PrefabEnd = "_PF";
+        public const string ComponentStart = "NC_";
+        public const string ComponentStartReplace = "C";
+
+        
+        public const string ProcessingDirectory = "Assets/HotUpdateResources/Prefab";
+        public const string ModuleScriptDirectory = "Program/HotLogic/HotUpdateFunction/Modules";
+        public static readonly string TemplatePath = Application.dataPath + "/Program/Platform/Editor/CreatModuleView/CreateModuleView.cs.txt";
+
+        
+        public static Dictionary<string, string> ComponentDic = new Dictionary<string, string>()
+        {
+            {"_TXT","Text" },
+            {"_IPF","InputField" },
+            {"_IMG","Image" },
+            {"_BTN","Button" },
+            {"_TOG","Toggle" },
+            {"_SLD","Slider"},
+            {"_TF","Transform" },
+            {"_RTF","RectTransform"},
+        };
+
+    }
+}
